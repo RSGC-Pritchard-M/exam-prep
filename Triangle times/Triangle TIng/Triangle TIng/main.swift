0f4bac8c5274be49917c1991c1662dc0cdbe8a0d
RSGC-Pritchard-M <Matthew@MacBook-Pro704-Pritchard.local>
2016-05-27 10:43:29 -0400

    First Commit

---
+++ Triangle times/Triangle TIng/Triangle TIng/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  Triangle TIng
+//
+//  Created by Matthew Pritchard on 2016-05-27.
+//  Copyright © 2016 Matthew Pritchard. All rights reserved.
+//
+
+import Foundation
+
+var angle1 = 0
+
+var angle2 = 0
+
+var angle3 = 0
+print("Hello, World!")
+
